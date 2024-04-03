258c54566920c67c7dea9d77c4a421493f1dbb36
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-04-03 20:11:40 +0530

    Update ImageService.java

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -1,3 +1,4 @@
+
 package com.driver.services;
 
 import com.driver.models.*;
@@ -5,7 +6,7 @@ import com.driver.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ImageService {
@@ -15,17 +16,40 @@ public class ImageService {
     @Autowired
     ImageRepository imageRepository2;
 
-    public Image addImage(Integer blogId, String description, String dimensions){
+    public Image addImage(Integer blogId, String description, String dimensions) {
         //add an image to the blog
+        Image image = new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
 
+        Blog blog = blogRepository2.findById(blogId).get();
+        image.setBlog(blog);
+
+        // add list to blog
+        blog.getImageList().add(image);
+        blogRepository2.save(blog);
+        return image;
     }
 
-    public void deleteImage(Integer id){
-
+    public void deleteImage(Integer id) {
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
-        //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        // Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        Image image = imageRepository2.findById(id).get();
+        int countImages = 0;
+        String[] screenSize = screenDimensions.split("X");
+        String[] imageSize = image.getDimensions().split("X");
 
+        // finding height and width of image
+        int imageH = Integer.parseInt(imageSize[0]);
+        int imageW = Integer.parseInt(imageSize[1]);
+
+        // finding height and width of screen
+        int screenH = Integer.parseInt(screenSize[0]);
+        int screenW = Integer.parseInt(screenSize[1]);
+
+        return (screenH / imageH) * (screenW / imageW);
     }
 }

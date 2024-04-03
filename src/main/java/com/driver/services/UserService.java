594e67da64dabc77822d257a223db1deda2a8fea
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-04-03 20:11:55 +0530

    Update UserService.java

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -1,3 +1,4 @@
+
 package com.driver.services;
 
 import com.driver.models.*;
@@ -7,22 +8,30 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository3;
 
-    public User createUser(String username, String password){
-
-
+    public User createUser(String username, String password) {
+        User user = new User(username, password);
+        userRepository3.save(user);
+        return user;
     }
 
-    public void deleteUser(int userId){
-
+    public void deleteUser(int userId) {
+        userRepository3.deleteById(userId);
     }
 
-    public User updateUser(Integer id, String password){
-
+    public User updateUser(Integer id, String password) {
+        Optional<User> userOptional = userRepository3.findById(id);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setPassword(password);
+            return userRepository3.save(user); // Return the updated user
+        }
+        return null;
     }
 }

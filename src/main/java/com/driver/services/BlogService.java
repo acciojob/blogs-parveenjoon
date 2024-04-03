33421d146c364ff025a732f5a1b2f6f941d3139f
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-04-03 20:11:27 +0530

    Update BlogService.java

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -1,3 +1,4 @@
+
 package com.driver.services;
 
 import com.driver.models.Blog;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BlogService {
@@ -22,12 +24,20 @@ public class BlogService {
     UserRepository userRepository1;
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
-        //create a blog at the current time
-
+        User user = userRepository1.findById(userId).get();
+        Blog blog = new Blog();
+        blog.setTitle(title);
+        blog.setUser(user);
+        blog.setPubDate(new Date());
+        blog.setContent(content);
+        user.getBlogList().add(blog);
+        blogRepository1.save(blog);
+        userRepository1.save(user);
+        return blog;
     }
 
-    public void deleteBlog(int blogId){
+    public void deleteBlog(int blogId) {
         //delete blog and corresponding images
-
+        blogRepository1.deleteById(blogId);
     }
 }
